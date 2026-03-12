50bdde451457c3aeb003446fa6c9dd7add5df358
Vũ Duy Hiếu <halilintarhieu@gmail.com>
2026-03-12 12:53:16 +0700

    Lab 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Contact } from './pages/contact/contact';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: Home,
+    },
+    {
+        path: 'contact',
+        component: Contact,
+    }
+];
