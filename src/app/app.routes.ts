6fe2443971ad9b2dade3e826c59161bc1867d681
Vũ Duy Hiếu <halilintarhieu@gmail.com>
2026-03-14 12:45:43 +0700

    Lab 3 hoàn chỉnh

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,34 @@
 import { Routes } from '@angular/router';
 import { Contact } from './pages/contact/contact';
 import { Home } from './home/home';
+import { About } from './pages/about/about';
+import { Products } from './pages/products/products';
+import { ProductDetail } from './pages/product-detail/product-detail';
+import { Stories } from './pages/stories/stories';
 
 export const routes: Routes = [
     {
-        path: 'home',
+        path: '',
         component: Home,
     },
     {
         path: 'contact',
         component: Contact,
+    },
+    {
+        path: 'about',
+        component: About,
+    },
+    {
+        path: 'products',
+        component: Products
+    }, 
+    {
+        path: 'products/:slug',
+        component: ProductDetail,
+    }, 
+    {
+        path: 'stories',
+        component: Stories
     }
 ];
---
+++ src/app/pages/stories/stories.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stories',
+  imports: [],
+  templateUrl: './stories.html',
+  styleUrl: './stories.css',
+})
+export class Stories {
+  stories = [
+    {
+      title: 'Dragon Ball',
+      releaseYear: '1984',
+      genre: 'Action',
+      image: 'https://upload.wikimedia.org/wikipedia/vi/4/4f/Dragon_Ball_Super_artwork.jpg',
+      views: 50000,
+    }, 
+    {
+      title: 'Attack On Titan',
+      releaseYear: '2009',
+      genre: 'Action',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSwu56DRuxFmg7dOM3Mlp3CO5i_4fiimtNTQ&s',
+      views: 90000,
+    },
+    {
+      title: 'Bleach',
+      releaseYear: '2001',
+      genre: 'Action',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuEMoIb0o2tl36SX-RKil4m6ThKQdflKjtcA&s',
+      views: 120000,
+    },
+  ];
+}
