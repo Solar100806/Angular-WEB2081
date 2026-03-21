ecf39e918897b18089afffdc914ad37d0636e1e8
Vũ Duy Hiếu <halilintarhieu@gmail.com>
2026-03-21 13:38:52 +0700

    Lab 6

--- src/app/pages/stories/stories.ts
+++ src/app/pages/stories/stories.ts
@@ -1,37 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+interface Story {
+  id: string | number;
+  title: string;
+  author: string;
+  views: number;
+}
 
 @Component({
   selector: 'app-stories',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './stories.html',
   styleUrl: './stories.css',
 })
-export class Stories {
-  stories = [
-    {
-      title: 'Dragon Ball',
-      releaseYear: '1984',
-      genre: 'Action',
-      image: 'https://upload.wikimedia.org/wikipedia/vi/4/4f/Dragon_Ball_Super_artwork.jpg',
-      views: 50000,
-    }, 
-    {
-      title: 'Attack On Titan',
-      releaseYear: '2009',
-      genre: 'Action',
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSwu56DRuxFmg7dOM3Mlp3CO5i_4fiimtNTQ&s',
-      views: 90000,
-    },
-    {
-      title: 'Bleach',
-      releaseYear: '2001',
-      genre: 'Action',
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuEMoIb0o2tl36SX-RKil4m6ThKQdflKjtcA&s',
-      views: 120000,
-    },
-  ];
+export class Stories implements OnInit {
+  private http = inject(HttpClient);
 
-  deleteStory(index: number) {
-    this.stories.splice(index, 1);
+  stories: Story[] = [];
+  isLoading = false;
+  isError = false;
+  deletingId: string | number | null = null;
+
+  private apiUrl = 'http://localhost:3001/stories';
+
+  ngOnInit() {
+    this.getStories();
+  }
+
+  getStories() {
+    this.isLoading = true;
+    this.isError = false;
+
+    this.http.get<Story[]>(this.apiUrl).subscribe({
+      next: (data) => {
+        this.stories = data;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error('Lỗi khi tải stories:', err);
+        this.isError = true;
+        this.isLoading = false;
+      },
+    });
+  }
+
+  deleteItem(id: string | number) {
+    if (!confirm('Bạn có chắc muốn xóa không?')) return;
+
+    this.deletingId = id;
+
+    this.http.delete(`${this.apiUrl}/${id}`).subscribe({
+      next: () => {
+        this.stories = this.stories.filter((s) => s.id !== id);
+        this.deletingId = null;
+      },
+      error: () => {
+        alert('Xóa thất bại!');
+        this.deletingId = null;
+      },
+    });
   }
 }
